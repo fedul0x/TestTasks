7bcd4f3dfc58e4b9fada520047ec0bfeb9d7ae5f
Алексей Ивашин <alexey.ivashin@nordclan.com>
2019-10-16 14:38:42 +0300

    reformat text variable value

--- src/main/java/com/nordclan/Main.java
+++ src/main/java/com/nordclan/Main.java
@@ -81,7 +81,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(getNumbers(100));
         System.out.println(combinations(3, 5));
-        String text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum";
+        String text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. " +
+                "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when" +
+                " an unknown printer took a galley of type and scrambled it to make a type specimen" +
+                " book. It has survived not only five centuries, but also the leap into electronic " +
+                "typesetting, remaining essentially unchanged. It was popularised in the 1960s with" +
+                " the release of Letraset sheets containing Lorem Ipsum passages, and more recently" +
+                " with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum";
         System.out.print(wordCounter(text));
     }
 }
